2c8cbb3065d45d270177f57e290a82efc1d7da78
albenchris <albenchris00@gmail.com>
2021-04-24 17:06:58 -0500

    commenting out files to attempt error fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,23 +1,23 @@
 const CACHE_NAME = 'budget-tracker-cache-v1';
 const DATA_CACHE_NAME = 'budget-data-cache-v1';
 const FILES_TO_CACHE = [
-    '/',
+    // '/',
     './index.html',
-    './manifest.json',
-    './css/styles.css',
+    // './manifest.json',
+    // './css/styles.css',
     
-    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-    './icons/icon-72x72.png',
-    './icons/icon-96x96.png',
-    './icons/icon-128x128.png',
-    './icons/icon-144x144.png',
-    './icons/icon-152x152.png',
-    './icons/icon-192x192.png',
-    './icons/icon-384x384.png',
-    './icons/icon-512x512.png',
+    // 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    // './icons/icon-72x72.png',
+    // './icons/icon-96x96.png',
+    // './icons/icon-128x128.png',
+    // './icons/icon-144x144.png',
+    // './icons/icon-152x152.png',
+    // './icons/icon-192x192.png',
+    // './icons/icon-384x384.png',
+    // './icons/icon-512x512.png',
 
-    './js/idb.js',
-    './js/index.js'
+    // './js/idb.js',
+    // './js/index.js'
 ];
 
 self.addEventListener('install', function (e) {
